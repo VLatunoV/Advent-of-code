66c90aaba32878a46868e9f90f7103a94636b782
Valentin Latunov <vlatunov@gmail.com>
2024-12-18 19:40:29 +0200

    Solve 2024 day13

--- 2024/day13/solve13.py
+++ 2024/day13/solve13.py
@@ -6,7 +6,6 @@ with open(input_file) as f:
 	lines = f.read().splitlines()
 
 def parse_bs(string):
-	print(string)
 	def parse_vector(single_line):
 		single_line = single_line.split(':')[1]
 		x, y = single_line.split(',')
@@ -20,15 +19,36 @@ def parse_bs(string):
 	a = parse_vector(string[0])
 	b = parse_vector(string[1])
 	prize = parse_vector(string[2])
-	print(a, b, prize)
+	return a, b, prize
 
 claws = [parse_bs(lines[i:i+4]) for i in range(0, len(lines), 4)]
 
+def dot(x, y):
+	return sum([x_i*y_i for x_i, y_i in zip(x, y)])
+def mul(a, x):
+	return tuple([x_i*a for x_i in x])
+def add(x, y):
+	return tuple([x_i+y_i for x_i, y_i in zip(x, y)])
+
 def solve_part1():
-	pass
+	ans = 0
+	for a, b, p in claws:
+		a_orth = (-a[1], a[0])
+		b_orth = (-b[1], b[0])
+		a_div = dot(b_orth, a)
+		b_div = dot(a_orth, b)
+		a_weight = dot(b_orth, p) / a_div if a_div != 0 else 0
+		b_weight = dot(a_orth, p) / b_div if b_div != 0 else 0
+		a_weight_int = round(a_weight)
+		b_weight_int = round(b_weight)
+		if p == add(mul(a_weight_int, a), mul(b_weight_int, b)):
+			ans += 3*a_weight_int + 1*b_weight_int
+	return ans
 
 def solve_part2():
-	pass
+	global claws
+	claws = [(a, b, add((10000000000000, 10000000000000), p)) for a,b,p in claws]
+	return solve_part1()
 
 print('Part 1:', solve_part1())
 print('Part 2:', solve_part2())
